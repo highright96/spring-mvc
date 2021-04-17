32b9c0652c0984a66bf52dad19ba4d62b942fb58
Sangwoo96 <nsw5296@naver.com>
2021-04-17 22:33:11 +0900

    [Add] 프로젝트 생성

---
+++ src/test/java/dev/highright96/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.highright96.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
